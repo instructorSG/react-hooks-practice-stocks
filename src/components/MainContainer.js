337aa375cd3b2434fb645909de4f871d6934da81
Sara <Sara.Ginsburg@flatironschool.com>
2022-07-28 11:48:00 -0400

    render all stocks onto the page

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [ stocks, setStocks ] = useState([])
+  const [portStocks, setPortStocks] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/stocks')
+      .then(res => res.json())
+      .then(data => setStocks(data))
+  }, [])
+console.log('stocks', stocks)//good
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={ stocks } />
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+  console.log('stocks2', stocks)//good
+  const stocksList = stocks.map((s) => <Stock key={ s.id } stock={ s } />)
+console.log('stocksList', stocksList)
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocksList}
     </div>
   );
 }
